364756ad2e7a8bcbdd2410d0b70e866b0e40a1aa
Cletor <lucaurssi@gmail.com>
2024-09-24 10:34:41 -0300

    OpenGl screen

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
+#ifdef __APPLE__
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
 
+
+using namespace std;
+
+#define windowWidth 900
+#define windowHeight 900
+
+
+void interface(){
+    return;
+}
+
+
+int main(int argc, char** argv){
+  
+    //----- Create Window -----//
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("TCC-Co-Evolucao-Formigas");
+    glClearColor(1.0, 1.0, 1.0, 1.0);// clear screen
+
+    glutDisplayFunc(interface);// defining drawing function
+
+    //glutTimerFunc(0, timer, 0); // clock, processing function here
+	//glutMouseFunc(button_click);// mouse click handler
+
+    glutMainLoop();
+    return 0;
+}
